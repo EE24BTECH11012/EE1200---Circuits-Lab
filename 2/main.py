49daad3f9f9432cef1162a7d0c7e3ebafd0a37f0
EE24BTECH11012 <bhavanisankargs@gmail.com>
2025-01-29 23:43:57 +0530

    commit

--- 2/main.py
+++ 2/main.py
@@ -9,9 +9,9 @@ def voltage(t, T) :
 
 
 
-R = 10 #Resistance
-C = 0.001 #Capacitance
-T = 1 #Time period
+R = 1000 #Resistance
+C = 0.00001 #Capacitance
+T = 0.01 #Time period
 tau = R*C
 h = 0.0005
 x = [0.0]
@@ -31,13 +31,15 @@ for i in range(10000) :
     k3 = h*(deriv(y[i] + h, x[i]))
     k4 = h*(deriv(y[i]+h, x[i]))
     y.append(y[i] + (1/6)*(k1 + 2*k2 + 2*k3 + k4))
+    if ( x[i] - 0.0042 ) < 0.0001 :
+        print(y[i])
         
 
 plt.plot(x, y)
 plt.grid(True)
 plt.xlabel('Time')
 plt.ylabel('Voltage')
-plt.savefig('fig1.png')
+#plt.savefig('fig1.png')
 plt.show()
 
 
